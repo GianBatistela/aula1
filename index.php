83cdde417826751f8edc868dad77b4a1b8671454
GianBatistela <admgian@live.com>
2018-03-01 21:16:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+02 <html>
+03 <head>
+04     <meta charset="UTF-8"/>
+05     <title>Document</title>
+06 </head>
+07 <body>
+08     <h1>TESTE DE COMIT</h1>
+09 </body>
+10 </html>
